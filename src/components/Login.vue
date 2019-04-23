ce6bbe2dd769b0460c96b18a897b29966b82207f
tsu-kaneko <tsu.kaneko0410@gmail.com>
2019-04-23 23:52:43 +0900

    ログイン画面と画面の初期表示の認証とAPI実行時にトークンを設定するよう追加

---
+++ src/components/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <h1>ログイン</h1>
+
+    <table class="table mx-auto w-50">
+      <tbody>
+      <tr>
+        <td>メールアドレス</td>
+        <td>
+          <input type="email" class="form-control" v-model="email" placeholder="hoge@domain.com">
+        </td>
+      </tr>
+      <tr>
+        <td>パスワード</td>
+        <td>
+          <input type="password" class="form-control" v-model="password" placeholder="password">
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+    <button @click="login">ログイン</button>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import router from '../router'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      let params = {
+        email: this.email,
+        password: this.password
+      }
+      axios
+      // TODO ドメインを環境ごとに切り分けたい
+        .post('http://localhost:8090/api/v1/login', params, {
+          xsrfHeaderName: 'X-XSRF-TOKEN',
+          withCredentials: true
+        })
+        .then(function (response) {
+          router.push('articles')
+        })
+        .catch((res) => {
+          console.error(res)
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+h1 {
+  border-bottom: 1px solid #eeeeee;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,50 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Search from '@/components/Search'
+import Login from '@/components/Login'
+import axios from 'axios'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/articles',
       name: 'search',
-      component: Search
+      component: Search,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    axios
+    // TODO ドメインを環境ごとに切り分けたい
+      .post('http://localhost:8090/api/v1/login/init', {}, {
+        xsrfHeaderName: 'X-XSRF-TOKEN',
+        withCredentials: true
+      })
+      .then(function (response) {
+        if (response.data) {
+          next()
+        } else {
+          // TODO ログイン画面に遷移先のURLを渡す
+          router.push('login')
+        }
+      })
+      .catch((res) => {
+        router.push('login')
+      })
+  } else {
+    next()
+  }
+})
+
+export default router
